c10198f226c15378c4befad1a830d4322b75eb0a
licb <545360100@qq.com>
2019-12-07 16:39:07 +0800

    feat: 🎸 init template

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>react template</div>
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));

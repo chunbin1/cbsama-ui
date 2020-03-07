080b28de8b766681d1d36bb3a133cfa3076795db
licb <545360100@qq.com>
2020-03-07 22:47:27 +0800

    feat: 🎸 发布keywords组件

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import RandomOrderArticles from "./components/RandomOrderArticles";
 // import BuildTest from "./components/BuildTest";
 import Loading from './components/Loading';
+import HighlightKeywords from './components/HighlightKeywords';
 
 // export { RandomOrderArticles, BuildTest };
-export { Loading };
+export { Loading, HighlightKeywords };
